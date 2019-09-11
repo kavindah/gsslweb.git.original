e6f2fa51415d7103bd6aab1783e77c023769ef73
kavindah <dskharshana@gmail.com>
2019-09-12 00:25:31 +0530

    db

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,31 +18,36 @@ use Illuminate\Support\Facades\Crypt;?>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <img class="d-block w-100" src="siteimages/1.jpg" alt="First slide" height="300px">
-                            <div class="carousel-caption d-none d-md-block" style="background: rgba(46, 51, 56, 0.4)!important;">
+                            <div class="carousel-caption d-none d-md-block"
+                                 style="background: rgba(46, 51, 56, 0.4)!important;">
                                 <h5> Feldspar quarry, Kaikawela, Matale, Sri Lanka</h5>
                             </div>
                         </div>
                         <div class="carousel-item">
                             <img class="d-block w-100" src="siteimages/2.jpg" alt="Second slide" height="300px">
-                            <div class="carousel-caption d-none d-md-block" style="background: rgba(46, 51, 56, 0.4)!important;">
-                                <h5>Migmatite, Dambulla, Sri Lanka</h5>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="siteimages/3.jpg" alt="Third slide" height="300px">
-                            <div class="carousel-caption d-none d-md-block" style="background: rgba(46, 51, 56, 0.4)!important;">
+                            <div class="carousel-caption d-none d-md-block"
+                                 style="background: rgba(46, 51, 56, 0.4)!important;">
                                 <h5>A pegmatitic vein cross cutting Bt-gneiss</h5>
                             </div>
                         </div>
                         <div class="carousel-item">
+                            <img class="d-block w-100" src="siteimages/3.jpg" alt="Third slide" height="300px">
+                            <div class="carousel-caption d-none d-md-block"
+                                 style="background: rgba(46, 51, 56, 0.4)!important;">
+                                <h5>A felsic dyke cross cutting Hbl-bt-gneiss</h5>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
                             <img class="d-block w-100" src="siteimages/4.jpg" alt="Third slide" height="300px">
-                            <div class="carousel-caption d-none d-md-block" style="background: rgba(46, 51, 56, 0.4)!important;">
-                                <h5>A felsic dyke cross cutting Hbi-bt-gneiss</h5>
+                            <div class="carousel-caption d-none d-md-block"
+                                 style="background: rgba(46, 51, 56, 0.4)!important;">
+                                <h5>Migmatite, Dambulla, Sri Lanka</h5>
                             </div>
                         </div>
                         <div class="carousel-item">
                             <img class="d-block w-100" src="siteimages/5.jpg" alt="Third slide" height="300px">
-                            <div class="carousel-caption d-none d-md-block" style="background: rgba(46, 51, 56, 0.4)!important;">
+                            <div class="carousel-caption d-none d-md-block"
+                                 style="background: rgba(46, 51, 56, 0.4)!important;">
                                 <h5>Weathered serpentinite, Ussangoda, Sri Lanka</h5>
                             </div>
                         </div>
@@ -67,141 +72,141 @@ use Illuminate\Support\Facades\Crypt;?>
         </div>
     </div>
 
-        <br>
+    <br>
 
 
-        <div class="row">
-            <div class="col-md-4">
-                <div style="background-color:#011947;width: 100%; height: 40px;border-radius:5px 5px 0px 0px;top: 0;">
-                    <h5 style="padding-top: 10px; text-align: center;color: white;">Highlights</h5>
-                </div>
-                <div style="background-color: white;height: 350px;overflow:hidden;overflow-y: auto">
-                    @if(count($highlights)>0)
-                        @foreach($highlights->take(1) as $highlight)
-                            <div style="padding-top: 1px;">
-                                {{--<div style="width: 100%;font-size: 16px;">{{ $focuse->title }}</div>--}}
-                                <div style="text-align: center;width: 100%;">
-                                    <img src="{{asset('highlight_images/'.$highlight->highlight_image)}}" width="100%">
-                                    <div style="padding: 5px 10px 10px 10px">
-                                        {!! $highlight->body !!}
-                                    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div style="background-color:#011947;width: 100%; height: 40px;border-radius:5px 5px 0px 0px;top: 0;">
+                <h5 style="padding-top: 10px; text-align: center;color: white;">Highlights</h5>
+            </div>
+            <div style="background-color: white;height: 350px;overflow:hidden;overflow-y: auto">
+                @if(count($highlights)>0)
+                    @foreach($highlights->take(1) as $highlight)
+                        <div style="padding-top: 1px;">
+                            {{--<div style="width: 100%;font-size: 16px;">{{ $focuse->title }}</div>--}}
+                            <div style="text-align: center;width: 100%;">
+                                <img src="{{asset('highlight_images/'.$highlight->highlight_image)}}" width="100%">
+                                <div style="padding: 5px 10px 10px 10px">
+                                    {!! $highlight->body !!}
                                 </div>
                             </div>
+                        </div>
 
-                            @if(!Auth::guest())
-                                @if(Auth::user()->id==$highlight->user_id)
-                                    <div class="row">
-                                        <div class="col-md-5">
+                        @if(!Auth::guest())
+                            @if(Auth::user()->id==$highlight->user_id)
+                                <div class="row">
+                                    <div class="col-md-5">
 
-                                            <a href="/highlights/{{$highlight->id}}/edit" class="btn membtn">Edit</a>
-                                        </div>
-                                        <div class="col-md-5">
-                                            {!!Form::open(['action'=>['highlightscontroller@destroy',$highlight->id],'method'=>'POST', 'class'=>'pull-right','onsubmit' => 'return ConfirmDelete()'])!!}
-                                            {{Form::hidden('_method','DELETE')}}
-                                            {{Form::submit('Delete',['class'=>'btn-danger','style'=>'width:95%;'])}}
-                                            {!!Form::close()!!}
-                                        </div>
+                                        <a href="/highlights/{{$highlight->id}}/edit" class="btn membtn">Edit</a>
                                     </div>
-                                @endif
-                            @endif
-                        @endforeach
-                    @else
-                        <p>No highlight here</p>
-                    @endif
-                </div>
-            </div>
-
-
-            <div class="col-md-4">
-                <div style="background-color:#641E16;width: 100%; height: 40px;border-radius:5px 5px 0px 0px;top: 0;">
-                    <h5 style="padding-top: 10px; text-align: center;color: white;">Focus</h5>
-                </div>
-                <div style="background-color: white;height: 350px;overflow:hidden;overflow-y: auto">
-                    @if(count($focuses)>0)
-                        @foreach($focuses->take(1) as $focuse)
-                            <div style="padding:1px;">
-                                <div style="text-align: center;width: 100%;">
-                                    <img src="{{asset('focus_images/'.$focuse->focus_image)}}" width="100%">
-                                    <div style="padding: 5px 10px 10px 10px">
-                                        {!! $focuse->body !!}
+                                    <div class="col-md-5">
+                                        {!!Form::open(['action'=>['highlightscontroller@destroy',$highlight->id],'method'=>'POST', 'class'=>'pull-right','onsubmit' => 'return ConfirmDelete()'])!!}
+                                        {{Form::hidden('_method','DELETE')}}
+                                        {{Form::submit('Delete',['class'=>'btn-danger','style'=>'width:95%;'])}}
+                                        {!!Form::close()!!}
                                     </div>
                                 </div>
-                            </div>
-
-                            @if(!Auth::guest())
-                                @if(Auth::user()->id==$focuse->user_id)
-                                    <div class="row">
-                                        <div class="col-md-5">
-
-                                            <a href="/focuses/{{$focuse->id}}/edit" class="btn membtn">Edit</a>
-                                        </div>
-                                        <div class="col-md-5">
-                                            {!!Form::open(['action'=>['focusescontroller@destroy',$focuse->id],'method'=>'POST', 'class'=>'pull-right','onsubmit' => 'return ConfirmDelete()'])!!}
-                                            {{Form::hidden('_method','DELETE')}}
-                                            {{Form::submit('Delete',['class'=>'btn-danger','style'=>'width:95%;'])}}
-                                            {!!Form::close()!!}
-                                        </div>
-                                    </div>
-                                @endif
                             @endif
-                        @endforeach
-                    @else
-                        <p>No post here</p>
-                    @endif
-                </div>
+                        @endif
+                    @endforeach
+                @else
+                    <p>No highlight here</p>
+                @endif
             </div>
+        </div>
 
 
-            <div class="col-md-4">
-                <div style="background-color: black;width: 100%; height: 40px;border-radius:5px 5px 0px 0px;top: 0;">
-                    <h5 style="text-align: center;color: white;padding-top:10px;">Events</h5>
-                </div>
-                <div style="background-color: white;height: 350px;">
-                    @if(count($calender)>0)
-                        @foreach($calender->take(1) as $calenders)
-                            <div style="padding: 10px">
-                                <a href="/calendar_event/{{$calenders->id}}">
-
-                                    <h3 class="display-10"><span class="badge badge-secondary"
-                                                                 style="background-color:#0E6655 ;"> {{ date('d M', strtotime($calenders->start_date)) }}</span>
-                                    </h3>
-                                    <p style="font-weight: bold;margin-left: 5px;text-align:left;">{!! $calenders->title !!}</p>
-
-                                    <div style="font-size: 10px;">{{ date('d M Y', strtotime($calenders->start_date)) }}</div>
-
-                                </a>
+        <div class="col-md-4">
+            <div style="background-color:#641E16;width: 100%; height: 40px;border-radius:5px 5px 0px 0px;top: 0;">
+                <h5 style="padding-top: 10px; text-align: center;color: white;">Focus</h5>
+            </div>
+            <div style="background-color: white;height: 350px;overflow:hidden;overflow-y: auto">
+                @if(count($focuses)>0)
+                    @foreach($focuses->take(1) as $focuse)
+                        <div style="padding:1px;">
+                            <div style="text-align: center;width: 100%;">
+                                <img src="{{asset('focus_images/'.$focuse->focus_image)}}" width="100%">
+                                <div style="padding: 5px 10px 10px 10px">
+                                    {!! $focuse->body !!}
+                                </div>
                             </div>
-                            @if(!Auth::guest())
-                                @if(Auth::user()->id==$calenders->user_id)
-                                    <div class="row">
-                                        <div class="col-md-5">
+                        </div>
 
-                                            <a href="/calendar_event/{{$calenders->id}}/edit"
-                                               class="btn membtn">Edit</a>
-                                        </div>
-                                        <div class="col-md-5">
-                                            {!!Form::open(['action'=>['calendercontroller@destroy',$calenders->id],'method'=>'POST', 'class'=>'pull-right','onsubmit' => 'return ConfirmDelete()'])!!}
-                                            {{Form::hidden('_method','DELETE')}}
-                                            {{Form::submit('Delete',['class'=>'btn-danger','style'=>'width:95%;'])}}
-                                            {!!Form::close()!!}
-                                        </div>
+                        @if(!Auth::guest())
+                            @if(Auth::user()->id==$focuse->user_id)
+                                <div class="row">
+                                    <div class="col-md-5">
+
+                                        <a href="/focuses/{{$focuse->id}}/edit" class="btn membtn">Edit</a>
                                     </div>
-                                @endif
+                                    <div class="col-md-5">
+                                        {!!Form::open(['action'=>['focusescontroller@destroy',$focuse->id],'method'=>'POST', 'class'=>'pull-right','onsubmit' => 'return ConfirmDelete()'])!!}
+                                        {{Form::hidden('_method','DELETE')}}
+                                        {{Form::submit('Delete',['class'=>'btn-danger','style'=>'width:95%;'])}}
+                                        {!!Form::close()!!}
+                                    </div>
+                                </div>
                             @endif
-                        @endforeach
-                    @else
-                        <p>No events here</p>
-                    @endif
-                    <hr style="background-color:#F5F5F5;height: 10px;margin: 0">
+                        @endif
+                    @endforeach
+                @else
+                    <p>No post here</p>
+                @endif
+            </div>
+        </div>
 
-                    <div style="bottom: 0">
-                        <iframe width="100%" height="57%" style="border: none" allowFullScreen
-                                src="https://www.youtube.com/embed/yzX4NTRoLVI?rel=0">
-                        </iframe>
-                    </div>
+
+        <div class="col-md-4">
+            <div style="background-color: black;width: 100%; height: 40px;border-radius:5px 5px 0px 0px;top: 0;">
+                <h5 style="text-align: center;color: white;padding-top:10px;">Events</h5>
+            </div>
+            <div style="background-color: white;height: 350px;">
+                @if(count($calender)>0)
+                    @foreach($calender->take(1) as $calenders)
+                        <div style="padding: 10px">
+                            <a href="/calendar_event/{{$calenders->id}}">
+
+                                <h3 class="display-10"><span class="badge badge-secondary"
+                                                             style="background-color:#0E6655 ;"> {{ date('d M', strtotime($calenders->start_date)) }}</span>
+                                </h3>
+                                <p style="font-weight: bold;margin-left: 5px;text-align:left;">{!! $calenders->title !!}</p>
+
+                                <div style="font-size: 10px;">{{ date('d M Y', strtotime($calenders->start_date)) }}</div>
+
+                            </a>
+                        </div>
+                        @if(!Auth::guest())
+                            @if(Auth::user()->id==$calenders->user_id)
+                                <div class="row">
+                                    <div class="col-md-5">
+
+                                        <a href="/calendar_event/{{$calenders->id}}/edit"
+                                           class="btn membtn">Edit</a>
+                                    </div>
+                                    <div class="col-md-5">
+                                        {!!Form::open(['action'=>['calendercontroller@destroy',$calenders->id],'method'=>'POST', 'class'=>'pull-right','onsubmit' => 'return ConfirmDelete()'])!!}
+                                        {{Form::hidden('_method','DELETE')}}
+                                        {{Form::submit('Delete',['class'=>'btn-danger','style'=>'width:95%;'])}}
+                                        {!!Form::close()!!}
+                                    </div>
+                                </div>
+                            @endif
+                        @endif
+                    @endforeach
+                @else
+                    <p>No events here</p>
+                @endif
+                <hr style="background-color:#F5F5F5;height: 10px;margin: 0">
+
+                <div style="bottom: 0">
+                    <iframe width="100%" height="57%" style="border: none" allowFullScreen
+                            src="https://www.youtube.com/embed/yzX4NTRoLVI?rel=0">
+                    </iframe>
                 </div>
             </div>
         </div>
+    </div>
     @include('layouts.footer')
 
 @endsection
--- resources/views/journal/browse/submit/index.blade.php
+++ resources/views/journal/browse/submit/index.blade.php
@@ -4,7 +4,7 @@
 @section('contents')
 <a href="/home" class="btn membtn" style="width: 20%;"><span class="glyphicon glyphicon-chevron-left"></span> Go To Dashboard</a><br>
 	<h3>Journal Submission</h3>
-	
+	<h2 style="color: red">Temporary unavailable of this feature </h2>
 	{!! Form::open(['action'=>'journalsubmitcontroller@store','method'=>'POST','enctype'=>'multipart/form-data' ]) !!}
 	<div class="form-group">
 		
